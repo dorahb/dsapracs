87c78e9a77fc2fa8414561db982438572da4a09d
BDNK <bdnk@BDNKs-MacBook-Air.local>
2023-07-20 10:06:01 +0300

    more fun stuff

---
+++ fizzbuzz.py
@@ -0,0 +1,13 @@
+def solution(number):
+    if number % 3 == 0 and number % 5 == 0:
+        return "FizzBuzz"
+    elif number % 3 == 0:
+        return "Fizz"
+    elif number % 5 == 0:
+        return "Buzz"
+    else:
+        return None
+
+number = int(input("Enter a number: "))
+result = solution(number)
+print("Result:", result)
