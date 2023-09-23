8e189f3dc5c7001330f454c8c5e1ccbf5add2e70
bianca <nyambura.bianca23@gmail.com>
2023-09-23 21:01:11 +0300

    More fun stuff

--- lambda.py
+++ lambda.py
@@ -8,3 +8,9 @@
 points = [(2, 5), (1, 3), (4, 1), (7, 6)]
 sorted_points = sorted(points, key=lambda x: x[1])
 print(sorted_points) 
+
+'''def add(a, b):
+    return a + b
+
+    add = lambda a, b: a + b
+'''
